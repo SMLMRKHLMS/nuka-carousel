c12340da1230b3354525d004899bb268a020cc7e
Andrew Vickory <AndrewVickory@iheartmedia.com>
2017-07-28 09:39:06 -0400

    clean

--- demo/app.js
+++ demo/app.js
@@ -22,7 +22,7 @@ const App = React.createClass({
           dragging={this.state.dragging}
           swiping={this.state.swiping}
         >
-          <a href="http://www.axs.com/series/4395/iheartradio-music-festival-tickets?skin=iheart" target="_blank" styles={{display:'block', width: '100%', height: '100%'}}><img src="http://placehold.it/1000x400&text=slide1"/></a>
+          <img src="http://placehold.it/1000x400&text=slide1"/>
           <img src="http://placehold.it/1000x400&text=slide2"/>
           <img src="http://placehold.it/1000x400&text=slide3"/>
           <img src="http://placehold.it/1000x400&text=slide4"/>
